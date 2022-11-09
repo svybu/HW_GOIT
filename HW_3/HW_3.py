e41cf2eb61108a1a7db5473501abbc0e56af17b7
svybu <svyatbutenko@gmail.com>
2022-11-09 14:25:07 +0200

    little changes
    little changes

--- HW_3/HW_3.py
+++ HW_3/HW_3.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 import calendar
 
-users = [{"name": "Bill", "birthday": datetime(year=2012, month=11, day=8)},
+users = [{"name": "Bill", "birthday": datetime(year=2012, month=11, day=17)},
          {"name": "Jill", "birthday": datetime(year=2009, month=11, day=12)},
-         {"name": "Kim", "birthday": datetime(year=2009, month=11, day=9)},
+         {"name": "Kim", "birthday": datetime(year=2009, month=11, day=11)},
          {"name": "Jan", "birthday": datetime(year=2009, month=11, day=13)},
-         {"name": "Jan__", "birthday": datetime(year=2009, month=11, day=22)}]
+         {"name": "Jan__", "birthday": datetime(year=2009, month=11, day=10)}]
 
 weekdays = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
 
@@ -16,21 +16,24 @@ def get_birthdays_per_week(users):
     сurrent_day = сurrent_day.date()
     week = timedelta(weeks=1)
     week += сurrent_day
-    for i in users_:
-        a = i["birthday"].strftime('%Y %B %d')
+    for user in users_:
+        a = user["birthday"].strftime('%Y %B %d')
         a = a.split(' ')
         a[0] = str(datetime.today().year)
         a[0] = ' '.join(a)
-        i["birthday"] = datetime.strptime(a[0], '%Y %B %d')
+        user["birthday"] = datetime.strptime(a[0], '%Y %B %d')
 
-        day_ = calendar.day_name[i["birthday"].weekday()]
-        if сurrent_day < i["birthday"].date() < week:
-            if calendar.day_name[i["birthday"].weekday()] in weekdays.keys():
-                weekdays[day_].append(i["name"])
+        day_ = calendar.day_name[user["birthday"].weekday()]
+        if сurrent_day < user["birthday"].date() < week:
+            if calendar.day_name[user["birthday"].weekday()] in weekdays.keys():
+                weekdays[day_].append(user["name"])
             else:
-                weekdays['Monday'].append(i["name"])
+                weekdays['Monday'].append(user["name"])
+    show_result(weekdays)
+
+def show_result(weekdays):
     for k, v in weekdays.items():
-        if len(v) != 0:
+        if v:
             v = ', '.join(v)
             print(f'{k}: {v}')
 
